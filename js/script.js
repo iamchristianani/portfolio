e3f703acb54b198570d25ba7a6e8d7d94702dc83
Christian Ani <iamchristianani@gmail.com>
2022-11-15 08:52:39 +0100

    Add mobile navigation menu also using JavaScript DOM

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const btn1 = document.querySelector('#menu-btn-1');
+const btn2 = document.querySelector('#menu-btn-2');
+const btnBox = document.querySelector('#menu-display');
+const portfolioBtn = document.querySelector('#work-btn');
+const aboutBtn = document.querySelector('#about-btn');
+const contactBtn = document.querySelector('#contact-btn');
