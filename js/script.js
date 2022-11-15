f918708392e06d62da5399239fa559130634d2a4
Christian Ani <iamchristianani@gmail.com>
2022-11-15 11:47:09 +0100

    Fix the backdrop on the mobile menu

--- js/script.js
+++ js/script.js
@@ -4,23 +4,34 @@ const btnBox = document.querySelector('#menu-display');
 const portfolioBtn = document.querySelector('#work-btn');
 const aboutBtn = document.querySelector('#about-btn');
 const contactBtn = document.querySelector('#contact-btn');
+const menuOverlay = document.querySelector('#menu-overlay');
 
 btn1.addEventListener('click', () => {
   btnBox.classList.toggle('menu-display');
+  menuOverlay.classList.toggle('menu-display');
 });
 
 btn2.addEventListener('click', () => {
   btnBox.classList.toggle('menu-display');
+  menuOverlay.classList.toggle('menu-display');
 });
 
 portfolioBtn.addEventListener('click', () => {
   btnBox.classList.toggle('menu-display');
+  menuOverlay.classList.toggle('menu-display');
 });
 
 aboutBtn.addEventListener('click', () => {
   btnBox.classList.toggle('menu-display');
+  menuOverlay.classList.toggle('menu-display');
 });
 
 contactBtn.addEventListener('click', () => {
   btnBox.classList.toggle('menu-display');
+  menuOverlay.classList.toggle('menu-display');
+});
+
+menuOverlay.addEventListener('click', () => {
+  btnBox.classList.toggle('menu-display');
+  menuOverlay.classList.toggle('menu-display');
 });
