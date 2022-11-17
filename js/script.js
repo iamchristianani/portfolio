91a6ff7c81937cb2797aecb8982e5f05d167aaec
Christian Ani <iamchristianani@gmail.com>
2022-11-17 10:25:44 +0100

    fix linter errors

--- js/script.js
+++ js/script.js
@@ -1,99 +1,122 @@
 const btnBox = document.querySelector('#menu-display');
 const modalContainer = document.querySelector('#modal-container');
-const modalCloseBtn = document.querySelector('#modal-close-btn');
 const workCard = document.getElementById('work-card-cont');
 const menuOverlay = document.querySelector('#menu-overlay');
+const menuButton = document.querySelector('#menu-btn-2');
+const hamburgerButton = document.querySelector('#hamburger-button');
+const workButton = document.querySelector('#work-btn');
+const aboutButton = document.querySelector('#about-btn');
+const contactButton = document.querySelector('#contact-btn');
 
-
-function show_menu() {
+function showMenu() {
   btnBox.classList.toggle('menu-display');
   menuOverlay.classList.toggle('menu-display');
 }
 
+menuButton.addEventListener('click', () => {
+  showMenu();
+});
+
+hamburgerButton.addEventListener('click', () => {
+  showMenu();
+});
+
+workButton.addEventListener('click', () => {
+  showMenu();
+});
+
+aboutButton.addEventListener('click', () => {
+  showMenu();
+});
+
+contactButton.addEventListener('click', () => {
+  showMenu();
+});
+
 const projects = [
   {
     id: 1,
-    name: "National Institute of Standards",
-    description: "Wearing face masks that adequately cover the mouth and nose causes the error rate of some of the most widely used facial recognition algorithms to spike to between 5 percent and 50 percent, a study by the US National Institute of Standards and Technology (NIST) has found.",
-    cardImage: "img/modal-image-mobile.png",
-    modalImage: "img/modal-image-mobile.png",
-    imageAlt: "National Institute of Standards",
-    technologies: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    liveLink: "https://iamchristianani.github.io/portfolio/",
-    sourceLink: "https://github.com/iamchristianani/portfolio"
+    name: 'National Institute of Standards',
+    description: 'Wearing face masks that adequately cover the mouth and nose causes the error rate of some of the most widely used facial recognition algorithms to spike to between 5 percent and 50 percent, a study by the US National Institute of Standards and Technology (NIST) has found.',
+    cardImage: 'img/modal-image-mobile.png',
+    modalImage: 'img/modal-image-mobile.png',
+    imageAlt: 'National Institute of Standards',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://iamchristianani.github.io/portfolio/',
+    sourceLink: 'https://github.com/iamchristianani/portfolio',
   },
   {
     id: 2,
-    name: "Free-phone Program",
-    description: "Chhattisgarhs $71 million free-phone program - known by the acronym SKY after its name in Hindi - is supposed to bridge the digital divide in this state of 26 million people, which is covered by large patches of forest and counts 7,000 villages that do not even have a wireless data signal.",
-    cardImage: "img/modal-image-mobile-2.png",
-    modalImage: "img/modal-image-mobile-2.png",
-    imageAlt: "Free-phone Program",
-    technologies: ["Ruby on Rails","CSS","JavaScript","HTML", "Python"],
-    liveLink: "https://iamchristianani.github.io/portfolio/",
-    sourceLink: "https://github.com/iamchristianani/portfolio"
+    name: 'Free-phone Program',
+    description: 'Chhattisgarhs $71 million free-phone program - known by the acronym SKY after its name in Hindi - is supposed to bridge the digital divide in this state of 26 million people, which is covered by large patches of forest and counts 7,000 villages that do not even have a wireless data signal.',
+    cardImage: 'img/modal-image-mobile-2.png',
+    modalImage: 'img/modal-image-mobile-2.png',
+    imageAlt: 'Free-phone Program',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML', 'Python'],
+    liveLink: 'https://iamchristianani.github.io/portfolio/',
+    sourceLink: 'https://github.com/iamchristianani/portfolio',
   },
   {
     id: 3,
-    name: "National Institute of Standards",
-    description: "Wearing face masks that adequately cover the mouth and nose causes the error rate of some of the most widely used facial recognition algorithms to spike to between 5 percent and 50 percent, a study by the US National Institute of Standards and Technology (NIST) has found.",
-    cardImage: "img/modal-image-mobile.png",
-    modalImage: "img/modal-image-mobile.png",
-    imageAlt: "National Institute of Standards",
-    technologies: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    liveLink: "https://iamchristianani.github.io/portfolio/",
-    sourceLink: "https://github.com/iamchristianani/portfolio"
+    name: 'National Institute of Standards',
+    description: 'Wearing face masks that adequately cover the mouth and nose causes the error rate of some of the most widely used facial recognition algorithms to spike to between 5 percent and 50 percent, a study by the US National Institute of Standards and Technology (NIST) has found.',
+    cardImage: 'img/modal-image-mobile.png',
+    modalImage: 'img/modal-image-mobile.png',
+    imageAlt: 'National Institute of Standards',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://iamchristianani.github.io/portfolio/',
+    sourceLink: 'https://github.com/iamchristianani/portfolio',
   },
   {
     id: 4,
-    name: "Free-phone Program",
-    description: "Chhattisgarhs $71 million free-phone program - known by the acronym SKY after its name in Hindi - is supposed to bridge the digital divide in this state of 26 million people, which is covered by large patches of forest and counts 7,000 villages that do not even have a wireless data signal.",
-    cardImage: "img/modal-image-mobile-2.png",
-    modalImage: "img/modal-image-mobile-2.png",
-    imageAlt: "Free-phone Program",
-    technologies: ["Ruby on Rails","CSS","JavaScript","HTML", "Python"],
-    liveLink: "https://iamchristianani.github.io/portfolio/",
-    sourceLink: "https://github.com/iamchristianani/portfolio"
+    name: 'Free-phone Program',
+    description: 'Chhattisgarhs $71 million free-phone program - known by the acronym SKY after its name in Hindi - is supposed to bridge the digital divide in this state of 26 million people, which is covered by large patches of forest and counts 7,000 villages that do not even have a wireless data signal.',
+    cardImage: 'img/modal-image-mobile-2.png',
+    modalImage: 'img/modal-image-mobile-2.png',
+    imageAlt: 'Free-phone Program',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML', 'Python'],
+    liveLink: 'https://iamchristianani.github.io/portfolio/',
+    sourceLink: 'https://github.com/iamchristianani/portfolio',
   },
   {
     id: 5,
-    name: "National Institute of Standards",
-    description: "Wearing face masks that adequately cover the mouth and nose causes the error rate of some of the most widely used facial recognition algorithms to spike to between 5 percent and 50 percent, a study by the US National Institute of Standards and Technology (NIST) has found.",
-    cardImage: "img/modal-image-mobile.png",
-    modalImage: "img/modal-image-mobile.png",
-    imageAlt: "National Institute of Standards",
-    technologies: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    liveLink: "https://iamchristianani.github.io/portfolio/",
-    sourceLink: "https://github.com/iamchristianani/portfolio"
+    name: 'National Institute of Standards',
+    description: 'Wearing face masks that adequately cover the mouth and nose causes the error rate of some of the most widely used facial recognition algorithms to spike to between 5 percent and 50 percent, a study by the US National Institute of Standards and Technology (NIST) has found.',
+    cardImage: 'img/modal-image-mobile.png',
+    modalImage: 'img/modal-image-mobile.png',
+    imageAlt: 'National Institute of Standards',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://iamchristianani.github.io/portfolio/',
+    sourceLink: 'https://github.com/iamchristianani/portfolio',
   },
   {
     id: 6,
-    name: "Free-phone Program",
-    description: "Chhattisgarhs $71 million free-phone program - known by the acronym SKY after its name in Hindi - is supposed to bridge the digital divide in this state of 26 million people, which is covered by large patches of forest and counts 7,000 villages that do not even have a wireless data signal.",
-    cardImage: "img/modal-image-mobile-2.png",
-    modalImage: "img/modal-image-mobile-2.png",
-    imageAlt: "Free-phone Program",
-    technologies: ["Ruby on Rails","CSS","JavaScript","HTML", "Python"],
-    liveLink: "https://iamchristianani.github.io/portfolio/",
-    sourceLink: "https://github.com/iamchristianani/portfolio"
-  }
-]
+    name: 'Free-phone Program',
+    description: 'Chhattisgarhs $71 million free-phone program - known by the acronym SKY after its name in Hindi - is supposed to bridge the digital divide in this state of 26 million people, which is covered by large patches of forest and counts 7,000 villages that do not even have a wireless data signal.',
+    cardImage: 'img/modal-image-mobile-2.png',
+    modalImage: 'img/modal-image-mobile-2.png',
+    imageAlt: 'Free-phone Program',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML', 'Python'],
+    liveLink: 'https://iamchristianani.github.io/portfolio/',
+    sourceLink: 'https://github.com/iamchristianani/portfolio',
+  },
+];
 
-function show_modal() {
+function showModal() {
   modalContainer.classList.toggle('modal-display');
 }
 
-function open_modal(projectIndex) {
+function openModal(projectIndex) {
   const oneProject = projects[projectIndex];
-  let list = "";
-  for (let items1 of oneProject.technologies) {
-    list += `<li>${items1}</li>`;
-  }
+  let list = '';
+  oneProject.technologies.forEach((item1) => {
+    list += `<li>${item1}</li>`;
+  });
 
   const modalContent = `<div class="modal">
   <div class="modal-image-box">
       <div class="modal-close-btn" id="modal-close-btn">
-          <i onclick="show_modal()" class="fa-solid fa-xmark"></i>
+          <i onclick="showModal()" class="fa-solid fa-xmark"></i>
       </div>
       <img src="${oneProject.modalImage}" alt="${oneProject.imageAlt}" class="modal-image-mobile">
   </div>
@@ -110,14 +133,14 @@ function open_modal(projectIndex) {
   </div>
   </div>`;
   modalContainer.innerHTML = modalContent;
-  show_modal();
+  showModal();
 }
 
-for (let items of projects) {
-  let list = "";
-  for (let items1 of items.technologies) {
-    list += `<li>${items1}</li>`;
-  }
+projects.forEach((items) => {
+  let list = '';
+  items.technologies.forEach((item1) => {
+    list += `<li>${item1}</li>`;
+  });
   const workCardContent = `<li class="work-card">
   <div class="word-card-img">
     <img src="${items.cardImage}" alt="${items.imageAlt}" class="modal-image-mobile">
@@ -131,14 +154,13 @@ for (let items of projects) {
   </div>
   </li>`;
   workCard.innerHTML += workCardContent;
-}
+});
 
 const projectButtonList = document.querySelectorAll('#project-buttons');
 
 projectButtonList.forEach((button) => {
   button.addEventListener('click', () => {
     const projectId = parseInt(button.dataset.id, 10) - 1;
-    open_modal(projectId);
-    console.log(projectId);
-  })
-})
+    openModal(projectId);
+  });
+});
